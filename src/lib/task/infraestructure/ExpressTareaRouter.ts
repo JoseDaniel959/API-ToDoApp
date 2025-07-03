bce9aafb363630516c69da76fca96e88e8b2397f
Jose Daniel <jobustamantea@unal.edu.co>
2025-07-03 14:20:20 -0500

    se añade delete

--- src/lib/task/infraestructure/ExpressTareaRouter.ts
+++ src/lib/task/infraestructure/ExpressTareaRouter.ts
@@ -9,5 +9,7 @@ const taskRouter = Router();
 taskRouter.get("/tasks/", controller.getAll)
 taskRouter.post("/tasks/",controller.create)
 taskRouter.get("/tasks/:id",controller.getTareaByID)
+taskRouter.delete("/tasks/:id",controller.deleteTareaBYID)
+
 
 export default taskRouter;
